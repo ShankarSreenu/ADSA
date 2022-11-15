5dee67997e352fe857e96f993ef223db147a20b7
ShankarSreenu <65806647+ShankarSreenu@users.noreply.github.com>
2022-11-15 16:49:18 +0530

    Update BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -15,7 +15,7 @@ class BubbleSort{
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
-            if (arr[j]>arr[j+1]){
+            if (arr[j]>=arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[i];
                 arr[i]=temp;
