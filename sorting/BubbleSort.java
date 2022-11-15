f763d2537d6bb5bf582f0924c7a0646a299a0cdd
ShankarSreenu <65806647+ShankarSreenu@users.noreply.github.com>
2022-11-15 16:07:22 +0530

    Create BubbleSort.java

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+//Bubble Sort 
+//compate jth, j+1 index elements,swap them if left is greater than right
+//After every iteration the greatest value is pushed to right side of array
+//In place sorting algorithm
+import java.*;
+import java.util.Scanner;  
+class BubbleSort{
+ public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr= new int[n];
+    for(int i=0;i<n;i++){
+        int val = sc.nextInt();
+        arr[i]=val;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+    }
+ }
+
+}
