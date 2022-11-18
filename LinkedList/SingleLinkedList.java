682be126c240ed32217d3d2d55ff3a428dd5af9f
ShankarSreenu <65806647+ShankarSreenu@users.noreply.github.com>
2022-11-18 09:45:55 +0530

    Update SingleLinkedList.java

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -1,38 +1,53 @@
-class node{
-    int val;
-    node next;
-    node(int val){
-        this.val=val;
-        this.next=null;
+
+public class linkedlist{
+
+    private class node{
+        int val;
+        node next;
+        node(int val){
+            this.val=val;
+            this.next=null;
+        }
     }
-}
-class linkedlist{
-    node head;
-    node tail;
+
+    private node head;
+    private node tail;
+    private int len;
+
     public void insert(int val){
           node obj = new node(val);
           if (this.head==null){
             this.head=obj;
+            len++;
           }
           else{
           this.tail.next=obj;
+          len++;
         }
         this.tail=obj;
     }
+
     public void traverse(){
         node temp=this.head;
         while(temp!=null){
-            System.out.println(temp.val);
+            System.out.println(temp.val+" ");
             temp=temp.next;
         }
     }
-}
-class Solution{
-  public static void main(String args[]){
-      linkedlist obj=new linkedlist();
-      obj.insert(1);
-      obj.insert(2);
-      obj.insert(3);
-      obj.traverse();
+    public int getlen(){
+        return this.len;
     }
+
+}
+
+class Solution{
+    public static void main(String args[]){
+        linkedlist obj=new linkedlist();
+        obj.insert(1);
+        obj.insert(2);
+        obj.insert(3);
+        obj.traverse();
+        System.out.println(obj.getlen());
+    }
+
 }
