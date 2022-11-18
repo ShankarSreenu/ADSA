1118ab0f7c7895da0ed8b8c10597a3c712e2fd06
ShankarSreenu <65806647+ShankarSreenu@users.noreply.github.com>
2022-11-18 09:39:40 +0530

    Update SingleLinkedList.java

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -1,1 +1,38 @@
-
+class node{
+    int val;
+    node next;
+    node(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+class linkedlist{
+    node head;
+    node tail;
+    public void insert(int val){
+          node obj = new node(val);
+          if (this.head==null){
+            this.head=obj;
+          }
+          else{
+          this.tail.next=obj;
+        }
+        this.tail=obj;
+    }
+    public void traverse(){
+        node temp=this.head;
+        while(temp!=null){
+            System.out.println(temp.val);
+            temp=temp.next;
+        }
+    }
+}
+class Solution{
+  public static void main(String args[]){
+      linkedlist obj=new linkedlist();
+      obj.insert(1);
+      obj.insert(2);
+      obj.insert(3);
+      obj.traverse();
+    }
+}
